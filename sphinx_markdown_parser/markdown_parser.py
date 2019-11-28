c5435a6e347b4a73a0a163b289e0dcfd8248f3ab
Ximin Luo <infinity0@pwned.gg>
2019-11-28 16:25:24 +0000

    hacky partial support of attr_list

--- sphinx_markdown_parser/markdown_parser.py
+++ sphinx_markdown_parser/markdown_parser.py
@@ -168,6 +168,14 @@ class MarkdownParser(parsers.Parser):
             return
         # shortcut for pushing one item so visitors don't have to
         if res is not None and res != self.parse_stack_w[-1]:
+            # add any leftover attributes to the docutils node.
+            # this is a slight hack to make attr_list "sort of work" - however
+            # docutils interprets attributes in its own way, not as html
+            # http://docutils.sourceforge.net/docs/ref/rst/directives.html
+            # e.g. style="" usually doesn't work, but some others do by chance
+            for (k, v) in node.attrib.items():
+                if k not in res:
+                    res[k] = v
             self.append_node(res)
         # add text
         if node.text and node.text.strip():
@@ -280,7 +288,8 @@ class MarkdownParser(parsers.Parser):
         return nodes.title()
 
     def visit_script(self, node):
-        if node.get("type", "").split(";")[0] == "math/tex":
+        if node.attrib.get("type", "").split(";")[0] == "math/tex":
+            node.attrib.pop("type")
             parent = self.parse_stack_r[-1]
             if parent.tag == "span":
                 return nodes.math()
@@ -301,6 +310,7 @@ class MarkdownParser(parsers.Parser):
 
     def visit_span(self, node):
         if "MathJax_Preview" in node.attrib.get("class", "").split():
+            node.attrib.pop("class")
             return IGNORE_ALL_CHILDREN
         return None
 
@@ -309,6 +319,7 @@ class MarkdownParser(parsers.Parser):
             # top-level, ignore
             return None
         if "MathJax_Preview" in node.attrib.get("class", "").split():
+            node.attrib.pop("class")
             return IGNORE_ALL_CHILDREN
         return None
 
@@ -341,7 +352,7 @@ class MarkdownParser(parsers.Parser):
 
     def visit_a(self, node):
         reference = nodes.reference()
-        href = node.attrib.get('href', '')
+        href = node.attrib.pop('href', '')
         if href.endswith(".md"):
             href = href[:-3] + ".html"
         reference['refuri'] = href
@@ -364,8 +375,8 @@ class MarkdownParser(parsers.Parser):
 
     def visit_img(self, node):
         image = nodes.image()
-        image['uri'] = node.attrib.get('src', '')
-        alt = node.attrib.get('alt', '')
+        image['uri'] = node.attrib.pop('src', '')
+        alt = node.attrib.pop('alt', '')
         if alt:
             image += nodes.Text(alt)
         return image
@@ -412,8 +423,9 @@ class MarkdownParser(parsers.Parser):
             # note: this isn't yet activated because fenced_code extension
             # outputs raw html block, not a regular markdown ast tree. instead
             # what is actually run is the hacky workaround in append_text
-            lang = node.get("class", "")
+            lang = node.attrib.get("class", "")
             if lang:
+                node.attrib.pop("class")
                 block["language"] = lang
             return block
         else:
